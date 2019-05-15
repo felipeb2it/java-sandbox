02cc5b3436c83d7ce3d8315c8e7c9334d9284a16
Felipe Gonçalves P. Oliveira <felipe.goliveira@ctis.com.br>
2019-05-15 18:04:08 -0300

    Limpeza de código e inclusão de Javadoc.

--- src/main/java/sandbox/util/Lambda.java
+++ src/main/java/sandbox/util/Lambda.java
@@ -22,6 +22,12 @@ public class Lambda {
 		funcionarios = funcs;
 	}
 	
+	
+	/**
+	 * Mapeia os nomes dos funcionários em uma lista de Strings.
+	 * @author felipe.goliveira
+	 * @return lista de Strings mapeadas.
+	 */
 	public List<String> mapToNomes(){
 		List<String> nomeFunc = funcionarios.stream()
 				.map(Funcionario::getNome)
@@ -29,6 +35,11 @@ public class Lambda {
 		return nomeFunc;
 	}
 	
+	/**
+	 * Filtra a lista de funcionários por idade e ordena por nome
+	 * @param idade
+	 * @return lista de funcionários filtrada e ordenada
+	 */
 	public List<Funcionario> getIdadeAcimaDe(Integer idade){
 		List<Funcionario> funcAlt = funcionarios.stream()
 				.filter(funcionario -> funcionario.getIdade() > idade)
@@ -37,6 +48,10 @@ public class Lambda {
 		return funcAlt;
 	}
 	
+	/**
+	 * Exemplo de funcionamento da interface Consumer
+	 * @return lista de funcionários.
+	 */
 	public List<Funcionario> functionalConsumer() {
 		List<Funcionario> copiaFuncionarios = new ArrayList<>();
 		copiaFuncionarios.addAll(funcionarios);
@@ -49,6 +64,12 @@ public class Lambda {
 		return copiaFuncionarios;
 	}
 	
+	/**
+	 * Gera um número aleatório com os limites de parâmetros min e max.
+	 * @param min
+	 * @param max
+	 * @return valor inteiro no intervalo.
+	 */
 	public Integer getRandom(int min, int max) {
 		Supplier<Integer> randomValue = () -> {
 			Random r = new Random();
